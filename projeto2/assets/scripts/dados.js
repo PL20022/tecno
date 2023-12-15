1a3c09d9aee5c4923fe18567057d1cbbf5513323
PL20022 <112514435+PL20022@users.noreply.github.com>
2023-12-15 19:42:11 -0300

    Add files via upload

---
+++ projeto2/assets/scripts/dados.js
@@ -0,0 +1,55 @@
+const dados = [
+  {
+    id: 1, // número de identificação do vídeo, sempre acrescentar +1 quando criar um novo objeto
+    video: "video_01.mp4", //nome do video, deve ser igual ao nome do arquivo na pasta videos
+    valor_premio: 34.7, //valor do premio em dinheiro  que aparece na tela
+
+    //perguntas que aparecem na entrevista, nao mudar estrutura e sempre escrever entre
+    //aspas duplas ("") e manter
+    pergunta1: "O vídeo é sobre futebol?",
+    pergunta2: "Nos primeiros 10 segundos tem gol?",
+    pergunta3: "O vídeo é +18?",
+  }, // isso aqui se chama "objeto", vc deve copiar tudo na área que
+  //abre e fecha chaves ({}), senao o código não vai funcionar
+
+  {
+    id: 2,
+    video: "video_02.mp4",
+    valor_premio: 38.0,
+
+    pergunta1: "O vídeo é sobre culinária?",
+    pergunta2: "O prato preparado é macarronada?",
+    pergunta3: "A receita é fácil de fazer?",
+  },
+
+  {
+    id: 3,
+    video: "video_03.mp4",
+    valor_premio: 46,
+
+    pergunta1: "O vídeo é sobre animais?",
+    pergunta2: "O animal do vídeo é um cachorro?",
+    pergunta3: "O cachorro do vídeo é da raça Husky Siberiano?",
+  },
+
+  //colar aqui
+];
+
+/*
+copiar daqui (não copiar este texto)
+
+{
+    id: 3, //alterar numero para o proximo da lista
+    video: "video_03.mp4", //nome do arquivo aqui
+    valor_premio: 46, //valor do premio que aparece na tela
+
+
+
+    pergunta1: "O vídeo é sobre animais?", //mudar somente texto entre aspas duplas
+    pergunta2: "O animal do vídeo é um cachorro?",
+    pergunta3: "O cachorro do vídeo é da raça Husky Siberiano?",
+  }, 
+  
+até aqui(não copiar este texto)
+
+  */
