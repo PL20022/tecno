af9123d54ebbd8f0066388f9c5de48f6e2fe65f2
PL20022 <112514435+PL20022@users.noreply.github.com>
2023-12-15 19:25:03 -0300

    Update dados.js

--- projeto/assets/scripts/dados.js
+++ projeto/assets/scripts/dados.js
@@ -1,35 +1,35 @@
 const dados = [
   {
     id: 1, // número de identificação do vídeo, sempre acrescentar +1 quando criar um novo objeto
-    video: "video_01.mp4", //nome do video, deve ser igual ao nome do arquivo na pasta videos
+    video: "-11_macaco.mp4", //nome do video, deve ser igual ao nome do arquivo na pasta videos
     valor_premio: 34.7, //valor do premio em dinheiro  que aparece na tela
 
     //perguntas que aparecem na entrevista, nao mudar estrutura e sempre escrever entre
     //aspas duplas ("") e manter
-    pergunta1: "O vídeo é sobre futebol?",
-    pergunta2: "Nos primeiros 10 segundos tem gol?",
-    pergunta3: "O vídeo é +18?",
+    pergunta1: "O vídeo é sobre animais?",
+    pergunta2: "tem 2 animais?",
+    pergunta3: "é um macaco em cima de um cachorro?",
   }, // isso aqui se chama "objeto", vc deve copiar tudo na área que
   //abre e fecha chaves ({}), senao o código não vai funcionar
 
   {
     id: 2,
-    video: "video_02.mp4",
+    video: "-21_apartamento.mp4",
     valor_premio: 38.0,
 
-    pergunta1: "O vídeo é sobre culinária?",
-    pergunta2: "O prato preparado é macarronada?",
-    pergunta3: "A receita é fácil de fazer?",
+    pergunta1: "O vídeo é sobre imoveis
+    pergunta2: "E de um apartamento?",
+    pergunta3: "custa mais de R$600 reais?",
   },
 
   {
     id: 3,
-    video: "video_03.mp4",
+    video: "-28.mp4",
     valor_premio: 46,
 
     pergunta1: "O vídeo é sobre animais?",
-    pergunta2: "O animal do vídeo é um cachorro?",
-    pergunta3: "O cachorro do vídeo é da raça Husky Siberiano?",
+    pergunta2: "O animal do vídeo é um gato?",
+    pergunta3: "ele esta no sol?",
   },
 
   //colar aqui
